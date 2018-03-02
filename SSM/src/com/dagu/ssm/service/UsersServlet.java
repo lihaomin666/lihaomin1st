693587e66d01a5a2a78267dceddbc4edcfa6d40e
lihaomin666 <lhm569922195@163.com>
2018-03-02 16:30:30 +0800

    修改之后的UserServlet

--- SSM/src/com/dagu/ssm/service/UsersServlet.java
+++ SSM/src/com/dagu/ssm/service/UsersServlet.java
@@ -26,8 +26,9 @@ public class UsersServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub修改了源代码
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
 	}
 
 	/**
